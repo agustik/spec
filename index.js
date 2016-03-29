6dd3fdaac5325b5e0ab25d4f7fa78b8d8eb047eb
Ágúst Ingi Kjartansson <agust.ingi.kjartansson@gmail.com>
2016-03-29 10:32:03 +0000

    Fo

--- index.js
+++ index.js
@@ -11,18 +11,36 @@ var Git = require("nodegit");
 
 
 // Set version manually?
-var version = argv.v || false;
+var _Version = argv.v || false;
 
 // Version bump?
-var bumpType = argv.t || 'patch';
+var _Release = argv.r || 'patch';
 
 // git tag it ?
-var bumpType = argv.g || false;
+var _GitTagIt = argv.g || false;
+
+// git tag it ?
+var _GitTagMessage = argv.m || "Tagged by specup";
+
+
+console.log({
+  version : _Version,
+  bump : _Release,
+  git : _GitTagIt
+});
 
 
 function readFile(_path){
   try {
     return fs.readFileSync(_path).toString('utf-8');
+  } catch (e) {
+    return false;
+  }
+}
+function writeFile(_path, content){
+  console.log(_path, 'Write');
+  try {
+    return fs.writeFileSync(_path, content);
   } catch (e) {
     return false;
   }
@@ -64,30 +82,68 @@ function setSpecVersion(specfile, version){
   return lines.join('\n')
 }
 
+function setNpmVersion(npm, version){
+
+  if (typeof npm === 'string'){
+    npm = JSON.parse(npm);
+  }
+
+  npm.version = version;
+
+  return JSON.stringify(npm, null, 2);
+}
+
+
+function SortTags(tags){
+  return tags.sort(function (a, b){
+    if(semver.gt(a,b)){
+      return 1;
+    }
+
+    if(semver.lt(a,b)){
+      return -1;
+    }
+
+    return 0;
+  });
+}
+function setNextTag(current){
+
+
+  return semver.inc(current, _Release);
+
+}
+
+function getLatestTag(tags){
+  var SortedTags = SortTags(tags);
+  return SortedTags.pop();
+}
+
 dir('.', function (err, list){
-  var specFile, npmFile, npmVersion, specVersion;
+  var specContent, specFileName, packageContent, npmVersion, specVersion;
 
   list.forEach(function (file){
     var parsed = path.parse(file);
 
     if (file === 'package.json'){
-      npmFile = fs.readFileSync(file).toString('utf8');
+      packageContent = fs.readFileSync(file).toString('utf8');
     }
 
     if (parsed.ext === '.spec'){
-     specFile = fs.readFileSync(file).toString('utf8');
+     specFileName = file;
+     specContent = fs.readFileSync(file).toString('utf8');
     }
   });
 
-  if (npmFile){
-    npmVersion = JSON.parse(npmFile).version;
+  if (packageContent){
+    npmVersion = JSON.parse(packageContent).version;
   }
-  if (specFile){
-    specVersion = getSpecVersion(specFile);
+  if (specContent){
+    specVersion = getSpecVersion(specContent);
   }
 
 
-  //setSpecVersion(specFile, '0.0.2');
+  //setSpecVersion(specContent, '0.0.2');
 
   var repo;
 
@@ -97,8 +153,43 @@ dir('.', function (err, list){
   });
 
   tags.then(function (listOfTags){
-    console.log(listOfTags);
-  })
+    var tag = getLatestTag(listOfTags);
 
+    var next = setNextTag(tag);
+
+    if (_Version){
+      next = _Version;
+    }
+
+    console.log(
+      {
+        spec : specVersion,
+        npm : npmVersion,
+        git_current : tag,
+        next : next
+      }
+    );
+
+
+    if (_GitTagIt){
+      repo.createTag(next, next, _GitTagMessage).then(function (res){
+        console.log(res);
+      })
+    }
+
+
+    if (specContent){
+      writeFile(
+        specFileName,
+        setSpecVersion(specContent, next)
+      )
+    }
+    if (packageContent){
+      writeFile(
+        'package.json',
+        setNpmVersion(packageContent, next)
+      )
+    }
+  });
 
 });
