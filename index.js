102a3b2c74a623c3cae85fe3cb39cfd8bbe587a3
Ágúst Ingi Kjartansson <agust.ingi.kjartansson@gmail.com>
2016-08-15 09:38:41 +0000

    added support for more than node

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ var spec = require('./lib/spec.js');
 
 var argv = require('yargs').argv;
 
+var type = argv.t || argv.type || 'node';
+
 
 switch (argv._[0]) {
   case 'up':
     spec.up();
     break;
   case 'init':
-    spec.init();
+    spec.init(type);
     break;
   default:
 
